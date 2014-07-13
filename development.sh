c8b3b519e8aa71529d586e4a57053fbfb46de834
Mitar <mitar.git@tnode.com>
2014-07-12 19:37:36 -0700

    We have to disable sendfile when running through boot2docker.
    See
    https://github.com/boot2docker/boot2docker/pull/284#issuecomment-48827833

--- development.sh
+++ development.sh
@@ -2,5 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker run -d --rm --name mysql -v "$DIR/development/db:/var/lib/mysql" cloyne/mysql-dev
-docker run -d --rm --name blog --link mysql:mysql -v "$DIR/development/etc:/etc/wordpress" -v "$DIR/plugins:/wordpress/wp-content/plugins" -v "$DIR/themes:/wordpress/wp-content/themes" -p 80:80 cloyne/blog
+docker run -d --name mysql -v "$DIR/development/db:/var/lib/mysql" -v "$DIR/development/log/mysql:/var/log/mysql" cloyne/mysql-dev
+docker run -d --name blog --link mysql:mysql -v "$DIR/development/etc/wordpress:/etc/wordpress" -v "$DIR/development/etc/service/nginx/run.initialization:/etc/service/nginx/run.initialization" -v "$DIR/plugins:/wordpress/wp-content/plugins" -v "$DIR/themes:/wordpress/wp-content/themes" -v "$DIR/development/log/nginx:/var/log/nginx" -p 80:80 cloyne/blog
