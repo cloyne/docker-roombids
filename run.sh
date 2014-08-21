3f24af182756add9d24b8bf65e8699ac613bac9f
Mitar <mitar.git@tnode.com>
2014-08-21 02:49:56 -0700

    Address.

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ mkdir -p /srv/var/log/blog
 
 # TODO: We should not trust whole Docker IP range
 
-docker run -d --name blog -h blog.blog.server2.docker -e SET_REAL_IP_FROM=172.17.0.0/16 -e ADMINADDR=root@cloyne.net -e REMOTES=mail.cloyne.net -v /srv/var/log/blog:/var/log/nginx -v /srv/blog/etc:/etc/wordpress -v /srv/blog/uploads:/wordpress/wp-content/uploads cloyne/blog
+docker run -d --name blog -h blog.blog.server2.docker -e SET_REAL_IP_FROM=172.17.0.0/16 -e ADMINADDR=root@cloyne.org -e REMOTES=mail.cloyne.net -v /srv/var/log/blog:/var/log/nginx -v /srv/blog/etc:/etc/wordpress -v /srv/blog/uploads:/wordpress/wp-content/uploads cloyne/blog
