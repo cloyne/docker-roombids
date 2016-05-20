4860e88ddd2189afe5071ffe50cf0e488c8d91ec
Mitar <mitar.git@tnode.com>
2016-05-20 00:19:37 -0700

    Use dockergen to serve HTTP.

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 
 mkdir -p /srv/var/log/blog
 
-# TODO: We should not trust whole Docker IP range
+# TODO: We should not trust whole Docker IP range.
 
-docker run -d --restart=always --name blog --hostname blog -e SET_REAL_IP_FROM=172.17.0.0/16 -e ADMINADDR=root@cloyne.org -e REMOTES=mail.cloyne.net -v /srv/var/hosts:/etc/hosts:ro -v /srv/var/log/blog:/var/log/nginx -v /srv/blog/etc:/etc/wordpress -v /srv/blog/uploads:/wordpress/wp-content/uploads cloyne/blog
+docker run --detach=true --restart=always --name blog --hostname blog \
+ --env VIRTUAL_HOST=cloyne.org --env VIRTUAL_ALIAS=/ \
+ --env SET_REAL_IP_FROM=172.17.0.0/16 --env ADMINADDR=root@cloyne.org --env REMOTES=mail.cloyne.net \
+ --volume /srv/var/hosts:/etc/hosts:ro --volume /srv/var/log/blog:/var/log/nginx \
+ --volume /srv/blog/etc:/etc/wordpress --volume /srv/blog/uploads:/wordpress/wp-content/uploads \
+ cloyne/blog
