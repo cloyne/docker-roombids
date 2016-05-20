cd305d195738652627971a907b5de2d40b7f0174
Mitar <mitar.git@tnode.com>
2016-05-20 01:07:57 -0700

    Enabled Let's encrypt.

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ mkdir -p /srv/var/log/blog
 # TODO: We should not trust whole Docker IP range.
 
 docker run --detach=true --restart=always --name blog --hostname blog \
- --env VIRTUAL_HOST=cloyne.org --env VIRTUAL_ALIAS=/ \
+ --env VIRTUAL_HOST=cloyne.org --env VIRTUAL_ALIAS=/ --env VIRTUAL_LETSENCRYPT=true \
  --env SET_REAL_IP_FROM=172.17.0.0/16 --env ADMINADDR=root@cloyne.org --env REMOTES=mail.cloyne.net \
  --volume /srv/var/hosts:/etc/hosts:ro --volume /srv/var/log/blog:/var/log/nginx \
  --volume /srv/blog/etc:/etc/wordpress --volume /srv/blog/uploads:/wordpress/wp-content/uploads \
